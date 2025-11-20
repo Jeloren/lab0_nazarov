84f3db1fcf73ff7e49759dd50dbc921a8251a052
МИША <jaroslavnazar80@gmail.com>
2025-11-20 15:32:13 +0300

    нужны срочно отчёты к лабам и ргр

---
+++ rgr1/lab8.asm
@@ -0,0 +1,73 @@
+; lab8.asm - Перемещение файла в Linux (x64)
+; Компиляция:
+; nasm -f elf64 lab8.asm -o lab8.o
+; ld lab8.o -o lab8
+
+section .data
+    err_msg db "Usage: ./lab8 <source> <target>", 10
+    err_len equ $ - err_msg
+    fail_msg db "Error: Could not move file.", 10
+    fail_len equ $ - fail_msg
+    ok_msg   db "Success: File moved.", 10
+    ok_len   equ $ - ok_msg
+
+section .text
+    global _start
+
+_start:
+    ; --- 1. Получение аргументов из стека ---
+    ; При старте стек выглядит так:
+    ; [rsp]      = argc (количество аргументов)
+    ; [rsp + 8]  = argv[0] (имя программы)
+    ; [rsp + 16] = argv[1] (исходный файл)
+    ; [rsp + 24] = argv[2] (новый путь)
+
+    pop rcx         ; Достаем argc в RCX
+    cmp rcx, 3      ; Должно быть 3 аргумента (программа + источник + цель)
+    jne print_usage ; Если не 3, выводим инструкцию
+
+    pop rdi         ; Пропускаем argv[0] (имя программы)
+    
+    pop rdi         ; argv[1] -> RDI (1-й аргумент sys_rename: oldname)
+    pop rsi         ; argv[2] -> RSI (2-й аргумент sys_rename: newname)
+
+    ; --- 2. Вызов sys_rename (номер 82) ---
+    ; int rename(const char *oldname, const char *newname);
+    mov rax, 82     ; Номер системного вызова sys_rename
+    syscall         ; Вызов ядра
+
+    ; --- 3. Проверка результата ---
+    test rax, rax   ; Если RAX < 0, значит ошибка
+    js move_failed
+
+    ; Успех
+    mov rax, 1          ; sys_write
+    mov rdi, 1          ; stdout
+    mov rsi, ok_msg
+    mov rdx, ok_len
+    syscall
+    jmp exit_app
+
+move_failed:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, fail_msg
+    mov rdx, fail_len
+    syscall
+    mov rdi, 1      ; Код выхода 1 (ошибка)
+    jmp do_exit
+
+print_usage:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, err_msg
+    mov rdx, err_len
+    syscall
+    mov rdi, 1
+
+exit_app:
+    mov rdi, 0      ; Код выхода 0 (ок)
+
+do_exit:
+    mov rax, 60     ; sys_exit
+    syscall
---
+++ rgr2/lab9.asm
@@ -0,0 +1,130 @@
+; lab9_fixed.asm - Управление клавиатурой через /dev/console
+; Сборка:
+; nasm -f elf64 lab9_fixed.asm -o lab9.o
+; ld lab9.o -o lab9
+; Запуск: sudo ./lab9
+
+section .data
+    dev_console db "/dev/console", 0
+    prompt_delay db "Enter Delay (ms) [250-1000]: ", 0
+    prompt_rate  db "Enter Period (ms) [33-500]: ", 0
+    msg_open_err db "Error: Cannot open /dev/console. Are you root?", 10, 0
+    msg_done     db "Settings applied via /dev/console!", 10, 0
+    
+    kb_struct:
+        dd 0 ; delay
+        dd 0 ; period
+
+    buffer db 32 dup(0)
+
+section .text
+    global _start
+
+_start:
+    ; --- 1. Открываем /dev/console ---
+    ; int open(const char *pathname, int flags);
+    mov rax, 2          ; sys_open
+    mov rdi, dev_console
+    mov rsi, 2          ; O_RDWR (чтение и запись)
+    mov rdx, 0
+    syscall
+
+    cmp rax, 0
+    jl open_failed      ; Если RAX < 0, ошибка открытия
+    mov r15, rax        ; Сохраняем дескриптор консоли в R15 (надежный регистр)
+
+    ; --- 2. Ввод Delay ---
+    mov rsi, prompt_delay
+    call print_string
+    call read_int
+    mov [kb_struct], eax 
+
+    ; --- 3. Ввод Period ---
+    mov rsi, prompt_rate
+    call print_string
+    call read_int
+    mov [kb_struct + 4], eax 
+
+    ; --- 4. Вызов ioctl KDKBDREP (0x4B52) ---
+    ; ioctl(fd, cmd, arg)
+    mov rax, 16         ; sys_ioctl
+    mov rdi, r15        ; ИСПОЛЬЗУЕМ ДЕСКРИПТОР /dev/console (а не 0)
+    mov rsi, 0x4B52     ; KDKBDREP
+    mov rdx, kb_struct
+    syscall
+
+    ; --- 5. Закрываем /dev/console ---
+    mov rax, 3          ; sys_close
+    mov rdi, r15
+    syscall
+
+    ; Успех
+    mov rsi, msg_done
+    call print_string
+    jmp exit_ok
+
+open_failed:
+    mov rsi, msg_open_err
+    call print_string
+
+exit_ok:
+    mov rax, 60
+    xor rdi, rdi
+    syscall
+
+; --- Процедуры (те же, что и раньше) ---
+print_string:
+    push rax
+    push rdi
+    push rdx
+    push rcx
+    mov rdx, 0
+.len_loop:
+    cmp byte [rsi + rdx], 0
+    je .print
+    inc rdx
+    jmp .len_loop
+.print:
+    mov rax, 1
+    mov rdi, 1
+    syscall
+    pop rcx
+    pop rdx
+    pop rdi
+    pop rax
+    ret
+
+read_int:
+    push rdi
+    push rsi
+    push rdx
+    push rcx
+    push rbx
+    mov rax, 0
+    mov rdi, 0      ; Читаем из stdin (клавиатуры пользователя)
+    mov rsi, buffer
+    mov rdx, 30
+    syscall
+    xor rax, rax
+    xor rcx, rcx
+    mov rbx, 10
+.next_digit:
+    movzx rdx, byte [buffer + rcx]
+    cmp rdx, 10
+    je .done
+    cmp rdx, '0'
+    jl .done
+    cmp rdx, '9'
+    jg .done
+    sub rdx, '0'
+    imul rax, rbx
+    add rax, rdx
+    inc rcx
+    jmp .next_digit
+.done:
+    pop rbx
+    pop rcx
+    pop rdx
+    pop rsi
+    pop rdi
+    ret
